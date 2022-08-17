d66e31be3ee1a8695a94690aa01697ca5c5e79e7
halitgorgulu <halitgorgulu01@gmail.com>
2022-08-17 16:25:39 +0300

    some test cases added.

--- src/test/java/com/heg/bookstoreapp/BookstoreappApplicationTests.java
+++ src/test/java/com/heg/bookstoreapp/BookstoreappApplicationTests.java
@@ -1,5 +1,6 @@
 package com.heg.bookstoreapp;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class BookstoreappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
