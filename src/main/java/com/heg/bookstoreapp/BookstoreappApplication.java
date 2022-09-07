ab3c23c693fd600b6b7e892fe92e5eab6425c0a1
halitgorgulu <halitgorgulu01@gmail.com>
2022-09-07 16:39:47 +0300

    cors added..

--- src/main/java/com/heg/bookstoreapp/BookstoreappApplication.java
+++ src/main/java/com/heg/bookstoreapp/BookstoreappApplication.java
@@ -22,7 +22,7 @@ public class BookstoreappApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://bookstoremanagementfrontend.herokuapp.com/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://bookstoremanagement.herokuapp.com/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
